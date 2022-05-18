a5b426a7e86d730eb93ac868cc9abc1a6a037bb9
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-05-19 01:35:31 +0900

    Monoid interface

---
+++ Monoid/Monoid.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime;
+
+namespace Monoid;
+public interface Monoid<T>
+{
+    public static T Empty { get; }
+
+    public static T Append(this T self, T value);
+}
