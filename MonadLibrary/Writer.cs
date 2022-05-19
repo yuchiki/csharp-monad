147ed7c5cebed676b641ab62b46c308eed6e18c6
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-05-19 11:21:46 +0900

    writer monad に return と bindを実装

---
+++ MonadLibrary/Writer.cs
@@ -0,0 +1,20 @@
+using Monoid;
+namespace Writer;
+
+public record Writer<W, T>(W Log, T Value)
+    where W : IMonoid<W>;
+
+public static class WriterExtension
+{
+    public static Writer<W, T> Return<W, T>(T value)
+        where W : IMonoid<W>
+        => new(W.Empty(), value);
+
+    public static Writer<W, T2> Bind<W, T1, T2>(Writer<W, T1> writer, Func<T1, Writer<W, T2>> f)
+        where W : IMonoid<W>
+    {
+        var (log, value) = f(writer.Value);
+        return new Writer<W, T2>(writer.Log.Append(log), value);
+    }
+
+}
